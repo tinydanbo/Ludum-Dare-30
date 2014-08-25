433b1d425ad1502ed7c3e826d19f31857fc08875
danbo <danbodaxter@gmail.com>
2014-08-25 22:35:22 +0100

    title screen control prompt

--- game/states/bonus.lua
+++ game/states/bonus.lua
@@ -1,14 +1,42 @@
 Gamestate = require "lib.hump.gamestate"
+Anim8 = require "lib.anim8"
 thanksState = require "game.states.thanks"
 
 local bonus = {}
 
 function bonus:enter(oldState)
 	self.elapsed = 0
+
+	self.backgroundSky = love.graphics.newImage("data/graphics/Background SKY.png")
+	self.backgroundSky:setFilter("nearest", "nearest")
+
+	self.backgroundFar = love.graphics.newImage("data/graphics/Background 2ndary.png")
+	self.backgroundFar:setFilter("nearest", "nearest")
+
+	self.backgroundNear = love.graphics.newImage("data/graphics/Background Animation2.png")
+	self.backgroundNear:setFilter("nearest", "nearest")
+
+	self.backgroundNearGrid = Anim8.newGrid(240, 160,
+		self.backgroundNear:getWidth(), self.backgroundNear:getHeight()
+	)
+
+	self.backgroundNearAnimation = Anim8.newAnimation(
+		self.backgroundNearGrid(
+			1, 1,
+			2, 1,
+			3, 1,
+			2, 1
+		), 0.5
+	)
+
+	self.pedestal = love.graphics.newImage("data/graphics/pedestal.png")
+	self.pedestal:setFilter("nearest", "nearest")
 end
 
 function bonus:update(dt)
 	self.elapsed = self.elapsed + dt
+
+	self.backgroundNearAnimation:update(dt)
 end
 
 function bonus:handleRescale()
@@ -24,6 +52,10 @@ end
 function bonus:draw()
 	love.graphics.push()
 		love.graphics.scale(scaleFactor, scaleFactor)
+		love.graphics.draw(self.backgroundSky, 0, 0)
+		love.graphics.draw(self.backgroundFar, 0, 0)
+		self.backgroundNearAnimation:draw(self.backgroundNear, 0, 0)
+		love.graphics.draw(self.pedestal, 0, 0)
 	love.graphics.pop()
 end
 
--- game/states/title.lua
+++ game/states/title.lua
@@ -1,4 +1,5 @@
 Gamestate = require "lib.hump.gamestate"
+Timer = require "lib.hump.timer"
 game = require "game.states.game" ---a hah ah ah ah aha
 
 local title = {}
@@ -6,6 +7,10 @@ local title = {}
 function title:enter(oldState)
 	self.titleScreen = love.graphics.newImage("data/graphics/screen_title.png")
 	self.titleScreen:setFilter("nearest", "nearest")
+	self.promptScreen = love.graphics.newImage("data/graphics/screen_prompt.png")
+	self.promptScreen:setFilter("nearest", "nearest")
+	self.state = 1
+	self.locked = false
 end
 
 function title:update(dt)
@@ -20,14 +25,30 @@ function title:keyreleased(key, code)
 	if key == "escape" then
 		love.event.quit()
 	else
-		Gamestate.push(game)
+		if not self.locked then
+			if self.state == 1 then
+				self.state = 2
+				self.locked = true
+				Timer.add(1, function()
+					self.locked = false
+				end)
+			else
+				Gamestate.push(game)
+				self.state = 1
+				self.locked = false
+			end
+		end
 	end
 end
 
 function title:draw()
 	love.graphics.push()
 		love.graphics.scale(scaleFactor, scaleFactor)
-		love.graphics.draw(self.titleScreen, 0, 0)
+		if self.state == 1 then
+			love.graphics.draw(self.titleScreen, 0, 0)
+		else
+			love.graphics.draw(self.promptScreen, 0, 0)
+		end
 	love.graphics.pop()
 end
 
